d934f550a67427ca4fe6739c1e3c4b6c47f0025e
domininik <dominik.fijas+github@gmail.com>
2022-11-14 19:58:51 +0100

    Initialize client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ethers } from 'ethers';
+import DFToken from './artifacts/contracts/DFToken.sol/DFToken.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contract: null,
+    signerAddress: '',
+    owner: null
+  }
+
+  componentDidMount() {
+    if (typeof window.ethereum !== 'undefined') {
+      this.initialize();
+    } else {
+      console.log('ERROR: web3 provider not available');
+    };
+  };
+
+  async initialize() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    let contract;
+    let signerAddress;
+
+    try {
+      signerAddress = await signer.getAddress();
+      const contractAddress = ethers.utils.getContractAddress({
+        from: signerAddress,
+        nonce: 0
+      });
+      contract = new ethers.Contract(contractAddress, DFToken.abi, signer);
+    } catch (error) {
+      console.log(error);
+    }
+
+    this.setState({
+      contract: contract,
+      signerAddress: signerAddress
+    });
+  }
+
+  render() {
+    return (
+      this.state.contract ? (
+        <div>Contract</div>
+      ) : (
+        <div>You are not authorized to access this page</div>
+      )
+    )
+  }
 }
 
 export default App;
