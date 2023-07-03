80eb2b7f9be90948e637780963986713c589e6ae
domininik <dominik.fijas+github@gmail.com>
2023-07-03 20:35:27 +0200

    Add test for cleaning bids

--- test/DFToken.js
+++ test/DFToken.js
@@ -210,6 +210,37 @@ describe("DFToken", function () {
       await expect(token.connect(otherAccount).buy(1, { value: 1000 })).not.to.be.reverted;
     });
 
+    it("Cleans trader's bids for this token", async function () {
+      const { token, owner, otherAccount } = await loadFixture(deployFixture);
+      await token.safeMint(owner.address, "");
+      await token.setPrice(1, 1000);
+      await token.connect(otherAccount).bid(1, 1000);
+      await token.approve(otherAccount.address, 1);
+      await token.connect(otherAccount).buy(1, { value: 1000 });
+
+      expect(await token.tokenBidsCount(1)).to.equal(0);
+      expect(await token.getBidsByToken(1)).to.be.empty;
+    });
+
+    it("Does not clean other trader's bids", async function () {
+      const { token, owner, otherAccount } = await loadFixture(deployFixture);
+      await token.safeMint(owner.address, "#1");
+      await token.safeMint(owner.address, "#2");
+      await token.setPrice(1, 1000);
+      await token.setPrice(2, 500);
+      await token.connect(otherAccount).bid(1, 1000);
+      await token.connect(otherAccount).bid(2, 500);
+      await token.approve(otherAccount.address, 1);
+      await token.connect(otherAccount).buy(1, { value: 1000 });
+
+      expect(await token.tokenBidsCount(1)).to.equal(0);
+      expect(await token.getBidsByToken(1)).to.be.empty;
+
+      expect(await token.tokenBidsCount(2)).to.equal(1);
+      const ids = await token.getBidsByToken(2);
+      expect(ids[0]).to.equal(1);
+    });
+
     it("Updates account ether balances", async function () {
       const { token, owner, otherAccount } = await loadFixture(deployFixture);
       await token.safeMint(owner.address, "");
